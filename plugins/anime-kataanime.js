052b8102fdec8fbf98e9e818baa494177f643daf
HamBotz <hambotz71@gmail.com>
2023-02-24 19:36:45 +0700

    ok

--- plugins/anime-kataanime.js
+++ plugins/anime-kataanime.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
   let res = await fetch('https://katanime.vercel.app/api/getrandom?limit=1')
-  if (!res.ok) throw await res.text()
+ // if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.result[0]) throw json
   let { indo, character, anime } = json.result[0]
